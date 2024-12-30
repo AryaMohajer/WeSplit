8947eb93f35b03ff61e15c36aa0846b8e5b21b14
arya <arya>
2024-12-30 17:08:41 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Arya Mohajer on 25/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            contentView()
+        }
+    }
+}
